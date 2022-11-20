06333f98df98875f736e1571fd11d81ff5dae600
Valnei Santos <valnei.silva@unidompedro.com>
2022-11-20 15:34:27 -0300

    menu consultar e cadastrar

---
+++ app/src/main/java/com/example/pharmacy/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.pharmacy;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+}
